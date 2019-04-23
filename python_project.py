e25c18c2562ca96fe757eec88950442c94440a92
likeeman <40840773+likeeman@users.noreply.github.com>
2019-04-23 18:34:23 +0300

    full app

--- python_project.py
+++ python_project.py
@@ -24,9 +24,60 @@ window.resizable(width=False, height=False) # запрещает юзеру из
 
 window.mainloop()'''
 from tkinter import *
+import json
 
 def exit():
     root.destroy()
+
+
+lst = []
+
+def add():
+    dictionary = {}
+    dictionary["task"] = entry_task.get()
+    dictionary["category"] = entry_category.get()
+    dictionary['time'] = entry_time.get()
+
+    lst.append(dictionary)
+
+
+    writer(lst)
+    #label_list_of_tasks.config(text=lst)
+    #label_list_of_tasks.config(text=[{"sdf":123, "23":432}])
+
+def show_list():
+    for todo in lst:
+        text_list_of_tasks.configure(state=NORMAL)
+        text_list_of_tasks.insert(1.0, "Задача: " + todo['task'] + " " + "Категория: " + todo['category'] + " " + "Дата: " +todo['time'] + '\n')
+        text_list_of_tasks.configure(state=DISABLED)
+
+    # сначала присваивает значениу NORMAL атрибуту state для возможности добавления текста
+    # после добавления текста изменяет значение на DISABLED для запрещения изменения
+    """text_list_of_tasks.configure(state=NORMAL)
+    text_list_of_tasks.insert(1.0, lst)
+    text_list_of_tasks.configure(state=DISABLED)"""
+
+
+def writer(something):
+    try:
+        with open('todo_list.json', 'w', encoding='windows-1251') as file_write:
+            json.dump(something, file_write, sort_keys=False, ensure_ascii=False)
+    except Exception as e:
+        print(e)
+
+
+def first_open_tasks():
+    try:
+        with open('todo_list.json', 'r', encoding='cp1251') as json_file:
+            contents = json.load(json_file)
+        for todo in contents:
+            text_list_of_tasks.configure(state=NORMAL)
+            text_list_of_tasks.insert(1.0, "Задача: " + todo['task'] + " " + "Категория: " + todo[
+                'category'] + " " + "Дата: " + todo['time'] + '\n')
+            text_list_of_tasks.configure(state=DISABLED)
+    except Exception as ex:
+        print(ex)
+
 
 
 root = Tk()
@@ -48,22 +99,52 @@ frame2.grid_propagate(0)
 # ширина одного фрейма = 160
 # высота равна 115
 
-text_list_of_tasks = Text(frame2, width=37, height=13).grid(row=0, column=0, padx=1, pady=1)
+text_list_of_tasks = Text(frame2, width=37, height=13)
+text_list_of_tasks.grid(row=0, column=0, padx=1, pady=1)
+
+
+"""#тестовый label для правого фрейма для вывода списка задач:
+label_list_of_tasks = Label(frame2, width=37, height=13, background='white')
+label_list_of_tasks.grid(row=0, column=0, padx=1, pady=2)"""
+
 
 # левый фрейм:
 
-label_task = Label(frame1, text='Задача: ', background='#D3D3D3').grid(row=0,column=0, pady=1)
-label_category = Label(frame1, text='Категория: ', background='#D3D3D3').grid(row=1, column=0, pady=1)
-label_time = Label(frame1, text='Время: ', background='#D3D3D3').grid(row=2, column=0, pady=1)
+label_task = Label(frame1, text='Задача: ', background='#D3D3D3')
+label_task.grid(row=0,column=0, pady=1)
 
-entry_task = Entry(frame1, width=40).grid(row=0, column=1, padx=5, pady=1)
-entry_category = Entry(frame1, width=40).grid(row=1, column=1)
-entry_time = Entry(frame1, width=40).grid(row=2, column=1)
+label_category = Label(frame1, text='Категория: ', background='#D3D3D3')
+label_category.grid(row=1, column=0, pady=1)
 
-button_add = Button(frame1, text="Добавить", width=10).grid(row=4,column=1, pady=1)
-button_list_of_tasks = Button(frame1, text="Список задач", width=13).grid(row=5,column=1, pady=1)
-button_exit = Button(frame1, text="Выход", width=8, command=exit).grid(row=6,column=1, pady=1)
+label_time = Label(frame1, text='Время: ', background='#D3D3D3')
+label_time.grid(row=2, column=0, pady=1)
 
+
+entry_task = Entry(frame1, width=40)
+entry_task.grid(row=0, column=1, padx=5, pady=1)
+
+
+entry_category = Entry(frame1, width=40)
+entry_category.grid(row=1, column=1)
+
+entry_time = Entry(frame1, width=40)
+entry_time.grid(row=2, column=1)
+
+
+button_add = Button(frame1, text="Добавить", width=10, command=add)
+button_add.grid(row=4,column=1, pady=1)
+
+button_list_of_tasks = Button(frame1, text="Список задач", width=13, command=show_list)
+button_list_of_tasks.grid(row=5,column=1, pady=1)
+
+button_exit = Button(frame1, text="Выход", width=8, command=exit)
+button_exit.grid(row=6,column=1, pady=1)
+
+
+
+
+
+first_open_tasks()
 
 
 root.resizable(width=False, height=False) # запрещает юзеру изменять размер окна
