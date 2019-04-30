58f262184f760e98fa517ca3769182c5898539f1
likeeman <40840773+likeeman@users.noreply.github.com>
2019-04-30 12:32:27 +0300

    удаление работает с багами, но без ошибок

--- python_project.py
+++ python_project.py
@@ -1,99 +1,105 @@
-'''from tkinter import *
-window = Tk()
-window.title("Менеджер задач")
-window.geometry('450x230')
-
-
-frame1 = Frame(window, background='black').pack()
-
-"""label1 = Label(text='Задача: ').grid(row=0, column=0)
-label2 = Label(text="Категория: ").grid(row=1, column=0)
-label3 = Label(text='Время:').grid(row=2, column=0)
-
-entry1 = Entry().grid(row=0, column=1)
-entry2 = Entry().grid(row=1, column=1)
-entry3 = Entry().grid(row=2, column=1)
-
-Entry1 = Entry(text='attempt').grid(row=0,column=3)"""
-
-
-
-
-
-window.resizable(width=False, height=False) # запрещает юзеру изменять размер окна
-
-window.mainloop()'''
 from tkinter import *
 import json
-
-def exit():
-    root.destroy()
-
+from tkinter import messagebox as mb
 
 lst = []
-
+lst_delete = []
 
 def add():
     dictionary = {}
-    dictionary["task"] = entry_task.get()
-    dictionary["category"] = entry_category.get()
-    dictionary['time'] = entry_time.get()
+    if entry_task.get() != '' and entry_category.get() != '' and entry_time.get() != '':
+        dictionary["task"] = entry_task.get()
+        dictionary["category"] = entry_category.get()
+        dictionary['time'] = entry_time.get()
+        lst.append(dictionary)
+        writer(lst)
+    elif entry_task.get() == '' and entry_category.get() != '' and entry_time.get() != '':
+        mb.showerror("Ошибка", "Вы не ввели задачу")
+    elif entry_task.get() != '' and entry_category.get() == '' and entry_time.get() != '':
+        mb.showerror("Ошибка", "Вы не ввели категорию")
+    elif entry_task.get() != '' and entry_category.get() != '' and entry_time.get() == '':
+        mb.showerror("Ошибка", "Вы не ввели время")
+    elif entry_task.get() == '' and entry_category.get() == '' and entry_time.get() != '':
+        mb.showerror("Ошибка", "Вы не ввели задачу и категорию")
+    elif entry_task.get() != '' and entry_category.get() == '' and entry_time.get() == '':
+        mb.showerror("Ошибка", "Вы не ввели категорию и время")
+    elif entry_task.get() == '' and entry_category.get() != '' and entry_time.get() == '':
+        mb.showerror("Ошибка", "Вы не ввели задачу и время")
+    else:
+        mb.showerror("Ошибка", "Вы ничего не ввели")
 
-    lst.append(dictionary)
-
-
-    writer(lst)
-
-    #label_list_of_tasks.config(text=lst)
-    #label_list_of_tasks.config(text=[{"sdf":123, "23":432}])
-
+    # label_list_of_tasks.config(text=lst)
+    # label_list_of_tasks.config(text=[{"sdf":123, "23":432}])
 
 
 def show_list():
-
+    text_list_of_tasks.configure(state=NORMAL)
+    text_list_of_tasks.delete(1.0, END)
     try:
+        button_ready_to_remove.grid_remove()
         button_delete_frame2.grid_remove()
         listbox_delete.grid_remove()
         text_list_of_tasks.grid()
-        for todo in lst:
+        with open('todo_list.json', 'r', encoding='cp1251') as json_file:
+            contents = json.load(json_file)
+        for todo in contents:
             text_list_of_tasks.configure(state=NORMAL)
             text_list_of_tasks.insert(1.0, "Задача: " + todo['task'] + " " + "Категория: " + todo[
                 'category'] + " " + "Дата: " + todo['time'] + '\n')
             text_list_of_tasks.configure(state=DISABLED)
+
     except:
-        for todo in lst:
+        with open('todo_list.json', 'r', encoding='cp1251') as json_file:
+            contents = json.load(json_file)
+        for todo in contents:
             text_list_of_tasks.configure(state=NORMAL)
-            text_list_of_tasks.insert(1.0, "Задача: " + todo['task'] + " " + "Категория: " + todo[
-                'category'] + " " + "Дата: " + todo['time'] + '\n')
+            text_list_of_tasks.insert(1.0, "Задача: " + str(todo['task']) + " " + "Категория: " + str(todo[
+                'category']) + " " + "Дата: " + str(todo['time']) + '\n')
             text_list_of_tasks.configure(state=DISABLED)
 
-
-
-
-    # сначала присваивает значениу NORMAL атрибуту state для возможности добавления текста
+    # сначала присваивает значениe NORMAL атрибуту state для возможности добавления текста
     # после добавления текста изменяет значение на DISABLED для запрещения изменения
     """text_list_of_tasks.configure(state=NORMAL)
     text_list_of_tasks.insert(1.0, lst)
     text_list_of_tasks.configure(state=DISABLED)"""
+    text_list_of_tasks.configure(state=DISABLED)
+
 
 def delete():
     for i in reversed(listbox_delete.curselection()):
         listbox_delete.delete(i)
+        #text_list_of_tasks.configure(state=NORMAL)
+        #text_list_of_tasks.delete(1.0, END)
+        #text_list_of_tasks.configure(state=DISABLED)
+        lst_delete.append(i)
+    print(lst_delete)
+def ready_to_delete():
+    global lst_delete
+
+    with open('todo_list.json', 'r', encoding='cp1251') as json_file:
+        contents = json.load(json_file)
+    for i in sorted(lst_delete, reverse=True):
+        contents.pop(i)
+        
+    with open('todo_list.json', 'w') as file_output_json:
+        json.dump(contents, file_output_json)
+    lst_delete = []
+
 
 
 def delete_task():
-    global button_delete_frame2
-    button_delete_frame2 = Button(frame2, text='Delete', command=delete)
-    button_delete_frame2.grid()
     global listbox_delete
-
+    global button_delete_frame2
+    listbox_delete.delete(0, END)
+    button_delete_frame2.grid()
+    button_ready_to_remove.grid()
     text_list_of_tasks.grid_remove()
-    listbox_delete = Listbox(frame2, selectmode=SINGLE, width=50)
     listbox_delete.grid(row=0, column=0, padx=1, pady=1)
-    for todo in lst:
+    with open('todo_list.json', 'r', encoding='cp1251') as json_file:
+        contents = json.load(json_file)
+    for todo in contents:
         listbox_delete.insert(END, "Задача: " + todo['task'] + " " + "Категория: " + todo['category'] + " " + "Дата: " +
                               todo['time'] + '\n')
-
 
 
 
@@ -118,18 +124,18 @@ def first_open_tasks():
         print(ex)
 
 
-def filter_tasks():
-    pass
+def exit():
+    root.destroy()
 
 
 root = Tk()
 root.title("Менеджер задач")
 
-frame1 = Frame(root, width=320, height=70, background="#D3D3D3") #default color was #D3D3D3
-frame2 = Frame(root, width=320, height=230, background="#D3D3D3") #default color was #D3D3D3
+frame1 = Frame(root, width=320, height=70, background="#2F2F2F")  # default color was #D3D3D3
+frame2 = Frame(root, width=320, height=231, background="#2F2F2F")  # default color was #D3D3D3
 
-#ФРЕЙМ ДЛЯ НОРМАЛЬНОГО РАСПОЛОЖЕНИЯ КНОПОК
-frame3 = Frame(root, width=320, height=160, background='#D3D3D3')
+# ФРЕЙМ ДЛЯ НОРМАЛЬНОГО РАСПОЛОЖЕНИЯ КНОПОК
+frame3 = Frame(root, width=320, height=161, background="#2F2F2F")
 
 frame1.grid(row=0, column=0)
 frame2.grid(row=0, column=1, rowspan=2)
@@ -148,28 +154,25 @@ frame3.grid_propagate(0)
 
 text_list_of_tasks = Text(frame2, width=37, height=13)
 text_list_of_tasks.grid(row=0, column=0, padx=1, pady=1)
-
+text_list_of_tasks.configure(state=DISABLED)
 
 """#тестовый label для правого фрейма для вывода списка задач:
 label_list_of_tasks = Label(frame2, width=37, height=13, background='white')
 label_list_of_tasks.grid(row=0, column=0, padx=1, pady=2)"""
 
-
 # левый фрейм:
 
-label_task = Label(frame1, text='Задача: ', background='#D3D3D3')
-label_task.grid(row=0,column=0, pady=1)
+label_task = Label(frame1, text='Задача: ', fg='white', bg="#2F2F2F")
+label_task.grid(row=0, column=0, pady=1)
 
-label_category = Label(frame1, text='Категория: ', background='#D3D3D3')
+label_category = Label(frame1, text='Категория: ', fg='white', bg="#2F2F2F")
 label_category.grid(row=1, column=0, pady=1)
 
-label_time = Label(frame1, text='Время: ', background='#D3D3D3')
+label_time = Label(frame1, text='Время: ', fg='white', bg="#2F2F2F")
 label_time.grid(row=2, column=0, pady=1)
-
 
 entry_task = Entry(frame1, width=40)
 entry_task.grid(row=0, column=1, padx=5, pady=1)
-
 
 entry_category = Entry(frame1, width=40)
 entry_category.grid(row=1, column=1)
@@ -177,37 +180,41 @@ entry_category.grid(row=1, column=1)
 entry_time = Entry(frame1, width=40)
 entry_time.grid(row=2, column=1)
 
+button_add = Button(frame3, text="Добавить", width=13, command=add, bg='#474747', fg='white',
+                    activebackground='#BFB173', relief='raised')
+# button_add.grid(row=4,column=1, pady=1)
+button_add.place(x=50, y=25)
 
-button_add = Button(frame3, text="Добавить", width=10, command=add)
-#button_add.grid(row=4,column=1, pady=1)
-button_add.place(x=50, y=10)
+button_delete = Button(frame3, text="Удалить задачу", width=13, command=delete_task, bg='#474747', fg='white',
+                       activebackground='#BFB173', relief='raised')
+# button_delete.grid(row=5, column=1)
+button_delete.place(x=50, y=55)
 
-button_delete = Button(frame3, text="Удалить задачу", command=delete_task)
-#button_delete.grid(row=5, column=1)
-button_delete.place(x=45, y=40)
+button_list_of_tasks = Button(frame3, text="Список задач", width=13, command=show_list, bg='#474747', fg='white',
+                              activebackground='#BFB173', relief='raised')
+# button_list_of_tasks.grid(row=7,column=1, pady=1)
+button_list_of_tasks.place(x=180, y=25)
 
-button_filter = Button(frame3, text="Фильтр задач", command=filter_tasks)
-#button_filter.grid(row=6, column=1)
-button_filter.place(x=188, y=40)
+button_exit = Button(frame3, text="Выход", width=13, command=exit, bg='#474747', fg='white',
+                     activebackground='#610303', relief='raised')
+# button_exit.grid(row=8,column=1, pady=1)
+button_exit.place(x=180, y=55)
 
-button_list_of_tasks = Button(frame3, text="Список задач", width=13, command=show_list)
-#button_list_of_tasks.grid(row=7,column=1, pady=1)
-button_list_of_tasks.place(x=180, y=10)
+button_delete_frame2 = Button(frame2, text='Delete', bg='#474747', fg='white', command=delete)
+button_delete_frame2.grid_remove()
 
-button_exit = Button(frame3, text="Выход", width=8, command=exit)
-#button_exit.grid(row=8,column=1, pady=1)
-button_exit.place(x=130, y=100)
+button_ready_to_remove = Button(frame2, text='Ready', command=ready_to_delete)
+button_ready_to_remove.grid_remove()
 
+listbox_delete = Listbox(frame2, selectmode=SINGLE, width=50, highlightcolor='white', bg='#E0E0E0',
+                             selectbackground='#2F2F2F')
+listbox_delete.grid_remove()
 
 first_open_tasks()
-
 
 # добавить функцию для фильтра по году или по алфавиту
 
 
-root.resizable(width=False, height=False)# запрещает юзеру изменять размер окна
+root.resizable(width=False, height=False)  # запрещает юзеру изменять размер окна
 
 root.mainloop()
-
-
-
